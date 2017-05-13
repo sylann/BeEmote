2eb28b63b7cf6cfb0ee2dd0e4d0e8dfd2fa29e3e
Romain VINCENT <romainfvincent@gmail.com>
2017-05-13 16:32:09 +0200

    Added public examples for the "security" files

---
+++ BeEmote.Services/Security/Example.ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿namespace BeEmote.Services.Example
+{
+    /// <summary>
+    /// Provides a connection string to use in the App.config file 
+    /// of any relevant Client."Interface_Project".
+    /// </summary>
+    internal static class ConnectionStrings
+    {
+        /// <summary>
+        /// This is an example connection string for Mysql.
+        /// Replace the following by your own values:
+        /// - Host_Name (server where the database is: either localhost or your_domain.com)
+        /// - Database_Name
+        /// - Database_User_Name
+        /// - Database_User_Password
+        /// </summary>
+        internal static string MysqlBeEmote
+        {
+            get => "Server=Host_Name; Database=Database_Name; Uid=Database_User_Name; Pwd=Database_User_Password;";
+        }
+    }
+}
---
+++ BeEmote.Services/Security/Example.Credentials.cs
@@ -0,0 +1,18 @@
+﻿namespace BeEmote.Services.Example
+{
+    /// <summary>
+    /// Provides the Microsoft's Cognitive Services activation Key
+    /// </summary>
+    class Credentials
+    {
+        /// <summary>
+        /// A valid activation key for the Emotion Api Cognitive Services.
+        /// </summary>
+        public static string EmotionKey { get => "Valid_Microsoft_Emotion_API_Key"; }
+
+        /// <summary>
+        /// A valid activation key for the Emotion Api Cognitive Services.
+        /// </summary>
+        public static string TextAnalyticsKey { get => "Valid_Microsoft_Text_Analytics_API_Key"; }
+    }
+}
