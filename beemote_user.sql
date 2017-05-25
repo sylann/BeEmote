3f7a27454426dbfaf44cd5da21a20458d8858983
Romain VINCENT <romainfvincent@gmail.com>
2017-05-25 16:42:46 +0200

    changed user creation method

--- beemote_user.sql
+++ beemote_user.sql
@@ -1,9 +1,4 @@
-USE mysql;
-
-DELETE FROM `user` WHERE `User` = 'beemote';
-DELETE FROM `db` WHERE `Db` = 'beemote' AND `User` = 'beemote';
-
-INSERT INTO `user` VALUES ('localhost','beemote','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','','','','',0,0,0,0,'mysql_native_password','*F88AF64F1D4762F2B3FCD678C228468E582543DD','N','2017-05-09 04:04:43',NULL,'N');
-INSERT INTO `db` VALUES ('localhost','beemote','beemote','Y','Y','Y','Y','Y','Y','N','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y');
+CREATE USER 'beemote'@'localhost' IDENTIFIED BY '?beemote';
+GRANT ALL ON beemote.* TO 'beemote'@'localhost';
 
 FLUSH PRIVILEGES;
