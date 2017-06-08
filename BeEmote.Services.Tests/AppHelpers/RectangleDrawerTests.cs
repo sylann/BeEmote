4882ad2a77baf27662940ee48a27db14084fcf97
Rascarat <ttheo38@gmail.com>
2017-06-08 11:50:11 +0200

    Little modification on ByteArrayBuilderTests.cs & RectangleDrawerTests.cs

--- BeEmote.Services.Tests/AppHelpers/RectangleDrawerTests.cs
+++ BeEmote.Services.Tests/AppHelpers/RectangleDrawerTests.cs
@@ -15,7 +15,7 @@ namespace BeEmote.Services.Tests
         private readonly RectangleDrawer drawer = new RectangleDrawer();
 
         [TestMethod()]
-        public void LeftResize_When_Return()
+        public void LeftResize_Change_With_ValidValues()
         {
             //Arrange
             int left = 20;
@@ -36,7 +36,28 @@ namespace BeEmote.Services.Tests
         }
 
         [TestMethod()]
-        public void TopResize_When_Return()
+        public void LeftResize_Change_With_InvalidValues()
+        {
+            //Arrange
+            int left = 5;
+            double canvaWidth = 2;
+            double imageWidth = 8;
+            double initialWidth = 4;
+            double offset = 3;
+            double result1;
+            double result2;
+
+            //Act
+            result1 = (canvaWidth - imageWidth) / 2;
+            result2 = offset + (left / initialWidth / imageWidth);
+
+            //Assert
+            Assert.AreNotEqual(result1, 1);
+            Assert.AreNotEqual(result2, 3.625);
+        }
+
+        [TestMethod()]
+        public void TopResize_Change_With_ValidValues()
         {
             //Arrange
             int top = 20;
@@ -57,7 +78,28 @@ namespace BeEmote.Services.Tests
         }
 
         [TestMethod()]
-        public void WidthResize_When_Return()
+        public void TopResize_Change_With_InvalidValues()
+        {
+            //Arrange
+            int top = 1;
+            double canvasHeight = 3;
+            double imageHeight = 8;
+            double initalHeight = 4;
+            double offset = 3;
+            double result1;
+            double result2;
+
+            //Act
+            result1 = (canvasHeight - imageHeight) / 2;
+            result2 = offset + (top / initalHeight / imageHeight);
+
+            //Assert
+            Assert.AreNotEqual(result1, 1);
+            Assert.AreNotEqual(result2, 3.625);
+        }
+
+        [TestMethod()]
+        public void WidthResize_Change_With_ValidValues()
         {
             //Arrange 
             double xValue = 10;
@@ -71,7 +113,21 @@ namespace BeEmote.Services.Tests
         }
 
         [TestMethod()]
-        public void HeightResize_When_Return()
+        public void WidthResize_Change_With_InvalidValues()
+        {
+            //Arrange 
+            double xValue = 3;
+            double initialWidth = 5;
+            double actualWidth = 2;
+            //Act 
+            double result = xValue / initialWidth / actualWidth;
+
+            //Assert
+            Assert.AreNotEqual(result, 1);
+        }
+
+        [TestMethod()]
+        public void HeightResize_Change_With_ValidValues()
         {
             //Arrange
             double yValue = 10;
@@ -83,6 +139,21 @@ namespace BeEmote.Services.Tests
 
             //Assert
             Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod()]
+        public void HeightResize_Change_With_InvalidValues()
+        {
+            //Arrange
+            double yValue = 2;
+            double initialHeight = 5;
+            double actualHeight = 2;
+
+            //Act
+            double result = yValue / initialHeight / actualHeight;
+
+            //Assert
+            Assert.AreNotEqual(result, 1);
         }
     }
 }
